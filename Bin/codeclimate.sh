c7204b54f2155275a4c2d869422248067d943ded
dp <denis.a.shestakov@gmail.com>
2015-01-13 14:59:59 +0300

    codeclimate

--- Bin/codeclimate.sh
+++ Bin/codeclimate.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-cd .. &&
-php ../_vendor/bin/test-reporter --stdout > ./Var/vendor/codeclimate/report.json &&
+CODECLIMATE_REPO_TOKEN=781407b639c8efcf74cdd6daadb7264131fb309562f73b4235dcccf4b250f6a3 ../_vendor/bin/test-reporter --coverage-report ./Var/vendor/phpunit/coverage.xml --stdout > ./Var/vendor/codeclimate/report.json &&
 curl -X POST -d @./Var/vendor/codeclimate/report.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
 
