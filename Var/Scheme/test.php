7ed4ff159f95db10cb64c4d6524ce301a0ba6893
dp <denis.a.shestakov@gmail.com>
2015-01-21 14:57:58 +0300

    fix scheme

--- Var/Scheme/test.php
+++ Var/Scheme/test.php
@@ -1,7 +1,43 @@
 <?php
 return [
-    'time' => '2015-01-20 12:39:30',
-    'revision' => '01201239',
+    'time' => '2015-01-12 15:08:43',
+    'revision' => '01121508',
     'scheme' => 'test',
-    'tables' => [],
+    'tables' => [
+        'ice_account' => [
+            'engine' => 'InnoDB',
+            'charset' => 'utf8_general_ci',
+            'comment' => '',
+            'modelClass' => 'Ice\\Model\\Account',
+            'revision' => '12281437',
+        ],
+        'ice_role' => [
+            'engine' => 'InnoDB',
+            'charset' => 'utf8_general_ci',
+            'comment' => '',
+            'modelClass' => 'Ice\\Model\\Role',
+            'revision' => '12281437',
+        ],
+        'ice_user' => [
+            'engine' => 'InnoDB',
+            'charset' => 'utf8_general_ci',
+            'comment' => '',
+            'modelClass' => 'Ice\\Model\\User',
+            'revision' => '12281437',
+        ],
+        'ice_user_role_link' => [
+            'engine' => 'InnoDB',
+            'charset' => 'utf8_general_ci',
+            'comment' => '',
+            'modelClass' => 'Ice\\Model\\User_Role_Link',
+            'revision' => '12281437',
+        ],
+        'ice_test' => [
+            'engine' => 'InnoDB',
+            'charset' => 'utf8_general_ci',
+            'comment' => '',
+            'modelClass' => 'Ice\\Model\\Test',
+            'revision' => '01081216',
+        ],
+    ],
 ];
