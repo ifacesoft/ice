0812d31b5a539e1786e3b00e70d51c59be226f78
dp <denis.a.shestakov@gmail.com>
2015-02-03 19:36:38 +0300

    fix travis

--- Bin/apc-install.sh
+++ Bin/apc-install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -e
+
+pecl install apcu
+
 if [ "$(expr "$TRAVIS_PHP_VERSION" "<" "5.5")" -eq 1 ]; then
 echo "extension = apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
