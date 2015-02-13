4b54c5ff768b93a9e6ea38f6e810ff6ce55bd5ff
dp <denis.a.shestakov@gmail.com>
2015-02-13 19:12:10 +0300

    fix travis

--- Bin/apc-install.sh
+++ Bin/apc-install.sh
@@ -4,4 +4,3 @@ echo "no" | pecl install channel://pecl.php.net/apcu-4.0.7
 echo "extension = redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension = apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-fi
