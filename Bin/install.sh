8abfe5afaaa2af0f8d90e54ef8bdaa75c720cc66
dp <denis.a.shestakov@gmail.com>
2015-02-13 19:33:33 +0300

    fix travis

--- Bin/install.sh
+++ Bin/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
-echo "no" | pecl install channel://pecl.php.net/redis-2.2.5
+
 echo "no" | pecl install channel://pecl.php.net/apcu-4.0.7
-echo "extension = redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension = apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+pyrus install pecl/redis && pyrus build pecl/redis
+echo "extension = redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
