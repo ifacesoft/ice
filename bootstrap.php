6d29c2dafb2943cefe4efb5bb237d83d4e279084
dp <denis.a.shestakov@gmail.com>
2015-03-16 16:55:10 +0300

    fix bootstarp

--- bootstrap.php
+++ bootstrap.php
@@ -1,27 +1,10 @@
 <?php
-
-if (defined('BOOTSTRAP')) {
-    return;
-}
-
-define('BOOTSTRAP', true);
-
 use Ice\Bootstrap;
 
-if (!defined('MODULE_DIR')) {
-    define('MODULE_DIR', __DIR__ . '/');
-}
+define('MODULE_DIR', __DIR__ . '/');
+define('MODULE_CONFIG_PATH', 'Config/Ice/Core/Module.php');
+define('VENDOR_DIR', realpath('../_vendor/Ice') . '/');
 
-$bootstrapPath = MODULE_DIR . '../Ice/Source/Ice/Bootstrap.php';
+$loader = require VENDOR_DIR . 'autoload.php';
 
-if (!file_exists($bootstrapPath)) {
-    $bootstrapPath = MODULE_DIR . 'Source/Ice/Bootstrap.php';
-
-    if (!file_exists($bootstrapPath)) {
-        die("\033[01;31mIce bootstrap not found! (" . $bootstrapPath . ")\033[0m\n");
-    }
-}
-
-require_once $bootstrapPath;
-
-Bootstrap::init();
+Bootstrap::init($loader);
