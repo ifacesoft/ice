20619701333a2afd866298182f60963a2b84b212
dp <dp@lab.ifacesoft.ru>
2015-03-18 01:26:07 +0300

    some fixes

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ if (!defined('ICE_BOOTSTRAP')) {
 
     define('MODULE_DIR', __DIR__ . '/');
     define('MODULE_CONFIG_PATH', 'Config/Ice/Core/Module.php');
-    define('VENDOR_DIR', realpath('../_vendor/Ice') . '/');
+    define('VENDOR_DIR', dirname(MODULE_DIR) . '/_vendor/Ice/');
 
     $loader = require VENDOR_DIR . 'autoload.php';
 
