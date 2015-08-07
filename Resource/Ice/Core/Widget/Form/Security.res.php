74a120acd2dc9b4b3f342da6343b8f07e8d4c29d
dp <denis.a.shestakov@gmail.com>
2015-08-07 19:41:58 +0300

    regenerate doctrine entities

---
+++ Resource/Ice/Core/Widget/Form/Security.res.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'Unknown accountModelClass' => [
+        'en' => 'Unknown accountModelClass',
+        'ru' => 'Неизвестно accountModelClass',
+    ],
+    'Validation failure' => [
+        'en' => 'Validation failure',
+        'ru' => 'Ошибки валидации',
+    ],
+    'error' => [
+        'en' => 'error',
+        'ru' => 'ошибка',
+    ],
+    'Account is expired' => [
+        'en' => 'Account is expired',
+        'ru' => 'Учетной записи истек',
+    ],
+    'User is blocked' => [
+        'en' => 'User is blocked',
+        'ru' => 'Пользователь заблокирован',
+    ],
+    'User is expired' => [
+        'en' => 'User is expired',
+        'ru' => 'Пользователь истек',
+    ],
+];
