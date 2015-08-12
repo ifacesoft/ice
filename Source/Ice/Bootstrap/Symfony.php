d5871045212ed225cb403970541f8fb5f5beb472
dp <denis.a.shestakov@gmail.com>
2015-08-12 12:25:32 +0300

    fixes

--- Source/Ice/Bootstrap/Symfony.php
+++ Source/Ice/Bootstrap/Symfony.php
@@ -18,8 +18,9 @@ class Symfony extends Bootstrap
     public function init(ClassLoader $loader, $force = true)
     {
         parent::init($loader, $force);
-        set_error_handler('Ice\Core\Logger::errorHandler');
-        register_shutdown_function('Ice\Core\Logger::shutdownHandler');
+
+//        set_error_handler('Ice\Core\Logger::errorHandler');
+//        register_shutdown_function('Ice\Core\Logger::shutdownHandler');
 
         $module = Module::getInstance();
 
