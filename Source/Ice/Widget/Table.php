24068adeac67ed7454bc15dfb1d61e5a4e2047d1
dp <denis.a.shestakov@gmail.com>
2015-10-26 20:31:41 +0300

    part of admin

--- Source/Ice/Widget/Table.php
+++ Source/Ice/Widget/Table.php
@@ -4,6 +4,31 @@ namespace Ice\Widget;
 
 use Ice\Core\Widget;
 
-abstract class Table extends Widget
+class Table extends Widget
 {
+    /**
+     * Widget config
+     *
+     * @return array
+     */
+    protected static function config()
+    {
+        return [
+            'render' => ['template' => true, 'class' => 'Ice:Php', 'layout' => null, 'resource' => null],
+            'access' => ['roles' => [], 'request' => null, 'env' => null, 'message' => 'Widget: Access denied!'],
+            'resource' => ['js' => null, 'css' => null, 'less' => null, 'img' => null],
+            'cache' => ['ttl' => -1, 'count' => 1000],
+            'input' => [],
+            'output' => [],
+        ];
+    }
+
+    /** Build widget
+     *
+     * @param array $input
+     * @return array
+     */
+    protected function build(array $input)
+    {
+    }
 }
