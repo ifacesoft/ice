0c00f63355206ee35df93fe31c9183c198fb2216
dp <denis.a.shestakov@gmail.com>
2015-11-24 16:56:46 +0300

    fix

--- Source/Ice/Model/Scheme.php
+++ Source/Ice/Model/Scheme.php
@@ -229,7 +229,7 @@ class Scheme extends Model
 		        'manyToOne' => [],
 		        'manyToMany' => [],
 		    ],
-		    'revision' => '11241045_d2j',
+		    'revision' => '11241356_vi4',
 		];
     }
 }
