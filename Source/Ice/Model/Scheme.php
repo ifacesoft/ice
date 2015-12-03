cdc9e01f1adbcdff199ebab86e9547275f6d077e
dp <denis.a.shestakov@gmail.com>
2015-12-03 19:08:45 +0300

    fix

--- Source/Ice/Model/Scheme.php
+++ Source/Ice/Model/Scheme.php
@@ -229,7 +229,7 @@ class Scheme extends Model
 		        'manyToOne' => [],
 		        'manyToMany' => [],
 		    ],
-		    'revision' => '12031552_hyp',
+		    'revision' => '12031600_ci7',
 		];
     }
 }
