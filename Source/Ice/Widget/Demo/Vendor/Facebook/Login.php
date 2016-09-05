ff2187f90dcc68c582b62bccbf6341ccbb1f922f
dp-ifacesoft <aB4gsavxmEP>
2016-09-05 15:46:16 +0300

    fix

---
+++ Source/Ice/Widget/Demo/Vendor/Facebook/Login.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dp
+ * Date: 05.09.16
+ * Time: 13:22
+ */
+
+namespace Ice\Widget;
+
+
+class Demo_Vendor_Facebook_Login extends Block
+{
+    protected static function config()
+    {
+        $config = parent::config();
+
+        $config['render']['template'] = __CLASS__;
+        $config['output']['appId'] = '121933101594886';
+
+        return $config;
+    }
+
+
+    protected function build(array $input)
+    {
+        return parent::build($input);
+    }
+}
