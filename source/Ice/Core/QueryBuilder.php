949713cf8fdf3ffe152d2309e72bbceab61735a6
dp-ifacesoft <denis.a.shestakov>
2020-10-07 12:29:13 +0300

    fix join union

--- source/Ice/Core/QueryBuilder.php
+++ source/Ice/Core/QueryBuilder.php
@@ -1165,7 +1165,6 @@ class QueryBuilder
         list($table, $tableAlias) = $this->getModelClassTableAlias($modelTableData);
 
         if ($table instanceof Query) {
-//                $table = $table->getQueryBuilder();
             $modelClass = $table->getQueryBuilder()->getModelClass();
         } else if ($table instanceof QueryBuilder) {
             $modelClass = $table->getModelClass();
@@ -1195,6 +1194,11 @@ class QueryBuilder
                 ? $join['class']->getModelClass()
                 : $join['class'];
 
+            // todo: это лишнее, ломает кейс джойна юниона - fix it
+            if (is_array($joinModelClass)) {
+                continue;
+            }
+
             $joinModelScheme = $joinModelClass::getScheme();
 
             $oneToMany = $joinModelScheme->gets('relations/' . ModelScheme::ONE_TO_MANY);
@@ -1217,6 +1221,12 @@ class QueryBuilder
                 ? $join['class']->getModelClass()
                 : $join['class'];
 
+
+            // todo: это лишнее, ломает кейс джойна юниона - fix it
+            if (is_array($joinModelClass)) {
+                continue;
+            }
+
             $joinModelScheme = $joinModelClass::getScheme();
 
             $manyToOne = $joinModelScheme->gets('relations/' . ModelScheme::MANY_TO_ONE);
@@ -1225,8 +1235,8 @@ class QueryBuilder
                 $this->sqlParts[self::PART_JOIN][$tableAlias] = [
                     'type' => $joinType,
                     'class' => $table,
-                    'on' => '`' . $tableAlias . '`.`' . $manyToOne[$modelClass] . '` = `' .
-                        $joinTableAlias . '`.`' . $joinModelClass::getPkColumnName() . '`'
+                    'on' => '`' . $tableAlias . '`.`' . $manyToOne[$modelClass] .
+                        '` = `' . $joinTableAlias . '`.`' . $joinModelClass::getPkColumnName() . '`'
                 ];
 
                 return true;
@@ -1238,6 +1248,11 @@ class QueryBuilder
             $joinModelClass = $join['class'] instanceof QueryBuilder || $join['class'] instanceof Query
                 ? $join['class']->getModelClass()
                 : $join['class'];
+
+            // todo: это лишнее, ломает кейс джойна юниона - fix it
+            if (is_array($joinModelClass)) {
+                continue;
+            }
 
             $joinModelScheme = $joinModelClass::getScheme();
 
