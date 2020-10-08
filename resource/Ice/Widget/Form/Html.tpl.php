0f42805cc7e3b896a2049d27b7b4e2005726b2cc
Ash <ash_williams@mail.ru>
2020-10-08 14:09:21 +0300

    (no branch): Auto stash before checking out "HEAD"

--- resource/Ice/Widget/Form/Html.tpl.php
+++ resource/Ice/Widget/Form/Html.tpl.php
@@ -23,7 +23,34 @@
         filebrowserBrowseUrl: '/ice/ckeditor/browse'
     });
 
+    CKEDITOR.config.extraPlugins = 'iframe';
+
+
     CKEDITOR.config.image_previewText = ' ';
+    CKEDITOR.editorConfig = function (config) {
+
+        config.toolbar = 'Full';
+        config.toolbar_Full =
+            [
+                { name: 'document', items: ['Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates'] },
+                { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
+                { name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt'] },
+                { name: 'forms', items: ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
+                        'HiddenField']
+                },
+                '/',
+                { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'] },
+                { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv',
+                        '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']
+                },
+                { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+                { name: 'insert', items: ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe'] },
+                '/',
+                { name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize'] },
+                { name: 'colors', items: ['TextColor', 'BGColor'] },
+                { name: 'tools', items: ['Maximize', 'ShowBlocks', '-', 'About'] }
+            ];
+    };
 
     for (var i in CKEDITOR.instances) {
         if (CKEDITOR.instances[i].name == '<?= $component->getId() ?>') {
