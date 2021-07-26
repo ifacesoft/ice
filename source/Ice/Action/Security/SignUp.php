001b5e86820bdedaf8bd9769a24dd0430e2c3f0b
dp-ifacesoft <denis.a.shestakov>
2021-07-26 14:23:15 +0300

    Security exception handler

--- source/Ice/Action/Security/SignUp.php
+++ source/Ice/Action/Security/SignUp.php
@@ -7,14 +7,22 @@ use Ice\Exception\DataSource_DuplicateEntry;
 use Ice\Exception\Not_Good;
 use Ice\Exception\Not_Valid;
 use Ice\Exception\Not_Verify;
-use Ice\Exception\Security_Account_AttachForbidden;
 use Ice\Widget\Account_Form;
 
 class Security_SignUp extends Security
 {
+    protected static function config()
+    {
+        $config = parent::config();
+
+        $config['input']['securityExceptionThrow'] = ['providers' => 'default', 'default' => 0];
+
+        return $config;
+    }
+
     /** Run action
      *
-     * @param  array $input
+     * @param array $input
      * @return array
      * @throws \Ice\Core\Exception
      */
@@ -33,13 +41,17 @@ class Security_SignUp extends Security
         } catch (Not_Good $e) {
             return ['error' => $logger->error('Плохие параметры', __FILE__, __LINE__, $e)];
         } catch (Not_Valid $e) {
-            return ['error' => $logger->error('Регистрационные данные не прошли валидацию: ' . $e->getMessage(), __FILE__, __LINE__, $e)];
+            return ['error' => $logger->error(['Регистрационные данные не прошли валидацию: {$0}', $e->getMessage()], __FILE__, __LINE__, $e)];
         } catch (Not_Verify $e) {
-            return ['error' => $logger->error('Запрос на регистрацию отклонен: ' . $e->getMessage(), __FILE__, __LINE__, $e)];
+            return ['error' => $logger->error(['Запрос на регистрацию отклонен: ', $e->getMessage()], __FILE__, __LINE__, $e)];
         } catch (DataSource_DuplicateEntry $e) {
             return ['error' => $logger->error('Пользователь или учетная запись уже существует', __FILE__, __LINE__, $e)];
-        } catch (Security_Account_AttachForbidden $e) {
-            return ['error' => $logger->error('Не удалось прикрепить аккаунт', __FILE__, __LINE__, $e)];
+        } catch (\Ice\Exception\Security $e) {
+            if ($input['securityExceptionThrow']) {
+                throw $e;
+            }
+
+            return ['error' => $logger->error(['Регистрация не удалась: {$0}', $e->getMessage()], __FILE__, __LINE__, $e)];
         } catch (\Exception | \Throwable $e) {
             return ['error' => $logger->error('При регистрации что-то пошло не так', __FILE__, __LINE__, $e)];
         }
