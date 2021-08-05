5505b952994e8310b570d60976642da493a2036c
dp-ifacesoft <denis.a.shestakov>
2021-08-05 16:09:25 +0300

    user not found 401

--- source/Ice/Exception/Security/User/NotActive.php
+++ source/Ice/Exception/Security/User/NotActive.php
@@ -2,7 +2,14 @@
 
 namespace Ice\Exception;
 
+use Ice\App;
+
 class Security_User_NotActive extends Security
 {
+    public function __construct($message, $errcontext = [], $previous = null, $errfile = null, $errline = null, $errno = 0)
+    {
+        parent::__construct($message, $errcontext, $previous, $errfile, $errline, $errno);
 
+        App::getResponse()->setStatusCode(401);
+    }
 }
